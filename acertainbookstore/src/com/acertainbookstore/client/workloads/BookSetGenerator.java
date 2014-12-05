fcb1016886350a6931b68943c417a3220e9dc887
calebllarson <caleblarson@Calebs-MacBook-Pro.local>
2014-12-05 01:30:24 +0100

    Implemented first pass Worker and BookSetGeneratorMethods.

--- acertainbookstore/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
+++ acertainbookstore/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
@@ -1,19 +1,24 @@
 package com.acertainbookstore.client.workloads;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Random;
+import java.util.LinkedList;
 import java.util.Set;
 
+import com.acertainbookstore.business.ImmutableStockBook;
 import com.acertainbookstore.business.StockBook;
+import com.acertainbookstore.interfaces.StockManager;
+import com.acertainbookstore.utils.BookStoreException;
+import com.sun.tools.javac.util.List;
 
 /**
  * Helper class to generate stockbooks and isbns modelled similar to Random
  * class
  */
 public class BookSetGenerator { 
-
+	
 	public BookSetGenerator() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -52,11 +57,28 @@ public class BookSetGenerator {
 	/**
 	 * Return num stock books. For now return an ImmutableStockBook
 	 * 
+	 * This method returns a random number of StockBook objects from the store associated with the passed in StockManager
+	 * 
 	 * @param num
 	 * @return
 	 */
-	public Set<StockBook> nextSetOfStockBooks(int num) {
-		// I do not understand what this is asking of us...
+	public Set<StockBook> nextSetOfStockBooks(StockManager stockManager, int num) {
+		
+		Set<StockBook> randomStockBooks = 	new HashSet<StockBook>();
+		ArrayList<StockBook> allStockBooks =  new ArrayList<StockBook>();
+		
+		try {
+			allStockBooks = (ArrayList<StockBook>) stockManager.getBooks();
+		} catch (BookStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < num; i++){
+			
+			randomStockBooks.add(allStockBooks.get(i));
+			
+		}
 		
 		return null;
 	}
--- acertainbookstore/src/com/acertainbookstore/client/workloads/Worker.java
+++ acertainbookstore/src/com/acertainbookstore/client/workloads/Worker.java
@@ -3,10 +3,21 @@
  */
 package com.acertainbookstore.client.workloads;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.Callable;
 
+import com.acertainbookstore.business.Book;
+import com.acertainbookstore.business.BookCopy;
+import com.acertainbookstore.business.StockBook;
+import com.acertainbookstore.interfaces.BookStore;
+import com.acertainbookstore.interfaces.StockManager;
 import com.acertainbookstore.utils.BookStoreException;
+import com.sun.tools.javac.util.List;
 
 /**
  * 
@@ -40,10 +51,10 @@ public class Worker implements Callable<WorkerRunResult> {
 				runRareStockManagerInteraction();
 			} else if (chooseInteraction < configuration
 					.getPercentFrequentStockManagerInteraction()) {
-				runFrequentStockManagerInteraction();
+				runFrequentStockManagerInteraction(configuration.getNumBooksWithLeastCopies(), configuration.getStockManager());
 			} else {
 				numTotalFrequentBookStoreInteraction++;
-				runFrequentBookStoreInteraction();
+				runFrequentBookStoreInteraction(configuration.getBookStore());
 				numSuccessfulFrequentBookStoreInteraction++;
 			}
 		} catch (BookStoreException ex) {
@@ -98,16 +109,117 @@ public class Worker implements Callable<WorkerRunResult> {
 	 * @throws BookStoreException
 	 */
 	private void runRareStockManagerInteraction() throws BookStoreException {
-		// TODO: Add code for New Stock Acquisition Interaction
+		
+		Set<StockBook> nextSetOfStockBooks = configuration.getBookSetGenerator().nextSetOfStockBooks(configuration.getStockManager(), configuration.getNumBooksToAdd());
+	
+		/* I'm confused here. In the assignment it says "It then checks if the set of ISBNs is in the
+		list of books fetched. I don't understand what set of ISBNS's the are talking about. I just created
+		a random one here (isbns) and implemented the logic explained in the assignment. */
+		
+		Set<Integer> isbns = new HashSet<Integer>();
+		Set<StockBook> toBeAdded = new HashSet<StockBook>(); 
+		
+		/* This for loop is implementing It then checks if the set of ISBNs is in the
+l		list of books fetched. Finally, it invokes addBooks with the set of books
+n		of found in the list returned by getBooks. */
+		
+		for (StockBook stockBook: nextSetOfStockBooks){
+			
+			if (!isbns.contains(stockBook.getISBN())){
+				
+				toBeAdded.add(stockBook);
+			}
+		}
+		
+		configuration.getStockManager().addBooks(toBeAdded);
 	}
 
 	/**
 	 * Runs the stock replenishment interaction
 	 * 
 	 * @throws BookStoreException
+	 * 
+	 * 
 	 */
-	private void runFrequentStockManagerInteraction() throws BookStoreException {
-		// TODO: Add code for Stock Replenishment Interaction
+	
+	/* Sorry, this method is really complicated. I'm sure there is an easier way to do this, but I couldn't 
+	 * think of one quickly. This method find the books with the k lowest copes, and then copies them the number
+	 * of times specified in Workload Configuration. 
+	 */
+	
+	private void runFrequentStockManagerInteraction(int k, StockManager stockManager) throws BookStoreException {
+		
+		@SuppressWarnings("unchecked")
+		Set<StockBook> allStockBooks = (Set<StockBook>) stockManager.getBooks();
+		Set<StockBook> toHaveCopiesAdded = new HashSet<StockBook>(); // holds books with k lowerst copies
+		
+		// findLowestCopy return the book with the lowest numberof copies
+		
+		for (int i = 0; i < k; i++){
+			
+			if (i == 0){ // The first iteration, the book with the lowest number of copies is found
+				
+				toHaveCopiesAdded.add(findLowestCopy(allStockBooks));
+			}
+			
+			/* If it is not the first iteration, the book with the lowest number of copies will be that found in the first iteration.
+			   This book must be removed from consideration. withOutLowest contains the number of books that 
+			   have not yet been found to be the lowest, and then findLowestCopy is called on that. 
+			*/
+			else {  
+				
+				Set<StockBook> withOutLowest = new HashSet<StockBook>();
+				withOutLowest = allStockBooks;
+				
+				for (StockBook stockBook: toHaveCopiesAdded){
+					
+					withOutLowest.remove(stockBook);
+				}
+				
+				toHaveCopiesAdded.add(findLowestCopy(withOutLowest));
+			}
+		}
+		
+		Set<BookCopy> copies = new HashSet<BookCopy>(); // This is what the StockManager will copy. 
+		
+		for (StockBook stockBook: toHaveCopiesAdded){
+			
+			BookCopy newCopy = new BookCopy(stockBook.getISBN(), configuration.getNumAddCopies());
+			copies.add(newCopy);
+		}
+		
+		stockManager.addCopies(copies);
+	}
+	
+	/* This method returns the book with the lowest number of copies and removes it from the set.*/
+	
+	private StockBook findLowestCopy(Set<StockBook> setOfStockBooks){
+		
+		int minimumCopies = 0;
+		StockBook bookWithMinimumCopies = null;
+		int i = 0;
+		
+		for (StockBook stockBook: setOfStockBooks){
+			
+			if (i == 0){
+				
+				minimumCopies = stockBook.getNumCopies();
+				bookWithMinimumCopies = stockBook;
+			}
+			
+			else if (stockBook.getNumCopies() <= minimumCopies){
+				
+				minimumCopies = stockBook.getNumCopies();
+				bookWithMinimumCopies = stockBook;
+			}
+			
+			else {
+				
+				continue;
+			}
+		}
+			
+		return bookWithMinimumCopies;
 	}
 
 	/**
@@ -115,8 +227,31 @@ public class Worker implements Callable<WorkerRunResult> {
 	 * 
 	 * @throws BookStoreException
 	 */
-	private void runFrequentBookStoreInteraction() throws BookStoreException {
-		// TODO: Add code for Customer Interaction
+	
+	/* I'm a little confused about this method because getEdirotPicks requires a number
+	 * paramter which I assume is configuration.getNumEditorPicksToGet(), but then getting 
+	 * a subset using sampleFromSetOfISBNS request another number parameter. I don't know 
+	 * what that parameter is.
+	 */
+	
+	private void runFrequentBookStoreInteraction(BookStore bookStore) throws BookStoreException {
+		
+		Set<Integer> isbns = new HashSet<Integer>();
+		
+		for(Book b: bookStore.getEditorPicks(configuration.getNumEditorPicksToGet())){
+			
+			isbns.add(b.getISBN());
+		}
+	
+		Set<Integer> toBuyIsbn = configuration.getBookSetGenerator().sampleFromSetOfISBNs(isbns, configuration.getNumEditorPicksToGet()); // I'm not sure if this parameter is correct. 
+		Set<BookCopy> bookCopyToBuy = new HashSet<BookCopy>();
+		
+		for (Integer isbn: toBuyIsbn){
+			
+			bookCopyToBuy.add(new BookCopy(isbn, configuration.getNumBooksToBuy()));
+		}
+		
+		bookStore.buyBooks(bookCopyToBuy);
 	}
 
 }
